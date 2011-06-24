4486a8b2883f7b9f53dc5c337edf9bef3134432d
unknown <Ezequiel@.(none)>
2011-06-24 01:57:36 -0300

    nuevos intentos de comunicacion spi

--- source/shiftgoback/shiftgoback.asm
+++ source/shiftgoback/shiftgoback.asm
@@ -16,17 +16,14 @@
 
 	.include "m88def.inc"
 	.org 0
-
+;defino constantes
 	.equ	pCSS	=	1
 	.equ	pSS		=	2
 	.equ	pMOSI	=	3
 	.equ	pMISO	=	4
 	.equ	pSCK	=	5
-
-
-
-	
-
+	.equ	Cslave =	0b11100000
+;defino simbolos
 	.def	tmp		=	r16
 	.def	arg		=	r17		;*	argument for calling subroutines
 	.def	rtn		=	r18		;*	return value from subroutines
@@ -34,8 +31,11 @@
 	.def	tmt		=	r20
 	.def	rcv		=	r21
 	.def	usd		=	r22
+	
 
 
+	.org 0x0000
+	.cseg
 	rjmp RESET
 
 ;*****************************************************************
@@ -66,124 +66,36 @@ RESET:
 ;*****************************************************************
 ;*	MAIN Program for microcontroller
 ;*****************************************************************
-MAIN:
-		;*	Inicio Micro1 como master
-		rcall SPI_Master_Init
-		rcall LCD_delay
-		rcall LCD_delay
-		rcall SPI_START
-		ldi tmt, 'h'
-		rcall SPI_Master_Transmit
-		rcall SPI_Wait_Transmit
-		rcall SPI_STOP
-		rcall SPI_SlaveInit
-		rcall wait_interrupt
+MAIN:	rcall	SPI_Sinit
+		rcall   idle	
 		
-		rcall SPI_Master_Init
-		rcall SPI_START
-		ldi tmt, 'o'
-		rcall SPI_Master_Transmit
-		rcall SPI_Wait_Transmit
-		rcall SPI_STOP
-		rcall SPI_SlaveInit
-		rcall wait_interrupt
-		rcall SPI_Master_Init
-		rcall SPI_START
-		ldi tmt, 'l'
-		rcall SPI_Master_Transmit
-		rcall SPI_Wait_Transmit
-		rcall SPI_STOP
-		rcall SPI_SlaveInit
-		rcall wait_interrupt
-		rcall SPI_Master_Init
-		rcall SPI_START
-		ldi tmt, 'a'
-		rcall SPI_Master_Transmit
-		rcall SPI_Wait_Transmit
-		rcall SPI_STOP
-		rcall SPI_SlaveInit
-		rcall wait_interrupt
-		rcall SPI_Master_Init
-		rjmp END_PROGRAM
 
 
-END_PROGRAM:
-		rjmp END_PROGRAM
+idle:	sei
+		rjmp idle
 
 
-;*****************************************************************
-;*	Configuración de la comunicación SPI en MASTER
-;*****************************************************************
-SPI_Master_Init:
-		;*	Set de SCK, MOSI y ~SS como salidas y MISO como
-		;*	entrada
-
-		cbi DDRB,pMISO
-		sbi	DDRB,pMOSI
-		sbi	DDRB,pSCK
-		cbi	DDRB,pSS
-		sbi	DDRB,pCSS
-		;*	Habilita comunicación SPI como MASTER a frecuencia
-		;*	de clock de f/16
-		ldi tmp, 0b01110001
-		out SPCR, tmp
-		ret
 
 
-;*****************************************************************
-;*Espero una interrupcion
-;*****************************************************************
-wait_interrupt:
-isit:	sei
-		cpi		 usd,0xF0
-		brne	isit
-		cli
-		ret
 
 ;*****************************************************************
 ;*	Configuración de la comunicación SPI en SLAVE
 ;*****************************************************************
-SPI_SlaveInit:
+SPI_Sinit:
 		;*	Set MISO output, all others input
 		sbi DDRB,pMISO
-		cbi	DDRB,pMOSI
-		cbi	DDRB,pSCK
-		cbi	DDRB,pSS
 		sbi	DDRB,pCSS
 		;*	Habilita SPI, como SLAVE
-		ldi r17,0b11100000
+		ldi r17,Cslave
 		out SPCR,r17
+		in	tmp,SPSR
+		in	tmp,SPDR
 		ret
 
-;*****************************************************************
-;*	Rutinas START/STOP del SPI
-;*****************************************************************
-SPI_START:
-		;*	Elijo el SLAVE con ~SS (PortB,2) en LOW
-		cbi PORTB, pCSS
-		ret
 
-SPI_STOP:
-		;*	Elijo el SLAVE con ~SS (PortB,2) en HIGH
-		sbi PORTB, pCSS
-		ret
 
-;*****************************************************************
-;*	Transmisión de 'tmt' por SPI al SLAVE
-;*****************************************************************
-SPI_Master_Transmit:
-		out	SPDR, tmt
-		ret
 
-;*****************************************************************
-;*	Espera del fin de la recepción SPI
-;*****************************************************************
-SPI_Wait_Transmit:
-		;*	Espera del fin de la recepción
-		in tmp, SPSR
-		sbrs tmp, SPIF
-		rjmp SPI_Wait_Transmit
-		ret
+
 
 ;*****************************************************************
 ;*	Rutina de delay para no pisar los bytes enviados
@@ -207,5 +119,6 @@ LCD_delay_inner:
 ;*****************************************************************
 SPI_STC:
 		in dta,SPDR
-		ldi 	usd,0xF0
+		inc dta
+		out	SPDR,dta
 		reti
