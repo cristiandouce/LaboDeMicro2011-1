6765063196a0741f36462d195571e378544630ee
cristiandouce <cristian@gravityonmars.com>
2011-07-01 17:50:30 -0300

    added SensorTry and ordered files inside shoots folder

---
+++ shoots/master_slave_shifted/CristianSensorTry/master/MASTER__SPI_LCD.asm
@@ -0,0 +1,442 @@
+/*
+ * spilcd_master_.asm
+ *
+ *  Created: 29/06/2011 19:31:21
+ *   Author: alumno
+ */ 
+
+
+ ;*****************************************************************
+;*	File: EFCmicro2.asm
+;*	
+;*	Espectrofotocolorimetro Microcontrolador 2
+;*	
+;*	Autores:
+;*			- Rodriguez Cañete, Macarena
+;*			- Pepe, Ezequiel Ignacio
+;*			- Douce Suárez, Cristian Gabriel
+;*
+;*	
+;*
+;*
+;*
+;*
+	.include "m88def.inc"
+
+
+;defino constantes
+	.equ	LCD_RS	= 1
+	.equ	LCD_RW	= 2
+	.equ	LCD_E	= 3
+
+	.equ	pCSS	=	1
+	.equ	pSS		=	2
+	.equ	pMOSI	=	3
+	.equ	pMISO	=	4
+	.equ	pSCK	=	5
+	.equ	Cmaster =	0b01110001
+	.equ	Cslave =	0b11100000
+
+;defino simbolos
+	.def	con		=	r23
+	.def	tmp		=	r16
+	.def	arg		=	r17		;*	argument for calling subroutines
+	.def	rtn		=	r18		;*	return value from subroutines
+	.def    dta		=	r19		;*	el q le da el master
+	.def	tmt		=	r20
+	.def	rcv		=	r21
+	.def	usd		=	r22
+
+;defino macros
+	.MACRO	SPI_START;*	Elijo el SLAVE con ~SS (PortB,2) en LOW
+		cbi PORTB, pSS
+	.ENDMACRO
+	.MACRO	SPI_STOP;*	Elijo el SLAVE con ~SS (PortB,2) en LOW
+		sbi PORTB, pSS
+	.ENDMACRO
+
+	.MACRO	SendInstruction;*	Envio Instruccion
+		ldi tmt,@0
+		rcall SPI_Mtransmit
+		rcall SPI_Wait
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		nop
+		in rcv,SPDR
+	.ENDMACRO
+	
+	.org 0x0300 ;defino una palabra para mandar
+		.cseg
+		string: .db "abcdefg"
+
+	.dseg
+		var:	.byte	6
+
+	.cseg
+	.org 0
+
+		rjmp RESET
+	
+
+;*****************************************************************
+;*	Defino los vectores de interrupcion
+;*****************************************************************
+	.org 0x011
+		rjmp SPI_STC ; SPI Transfer Complete Handler
+
+
+
+;*****************************************************************
+;*	Inicialización del Micro luego del RESET
+;*****************************************************************
+RESET:
+		ldi	tmp, low(RAMEND)
+		out	SPL, tmp
+		ldi	tmp, high(RAMEND)
+		out	SPH, tmp
+		rjmp MAIN
+
+
+;*****************************************************************
+;*	MAIN Program for microcontroller
+;*****************************************************************
+MAIN:
+		
+		;*	Habilito el LCD
+		rcall	LCD_init
+		;*	Espero la busy flag
+	    rcall	LCD_wait
+		;*Inicio el SPI como Master
+		rcall 	SPI_Minit
+		
+		rcall SlaveSensorInit
+		rcall PutSensorData
+
+jm:		rjmp jm
+		
+		
+		SPI_START
+		rcall	SPI_Sendstring
+		SPI_STOP
+		rcall	LCD_Putstring
+loop:	rjmp	loop
+
+SlaveSensorInit:
+		SPI_START
+		SendInstruction 's'
+		SPI_STOP
+		rcall DELAY
+		ret
+
+PutSensorData:
+		ldi con,5
+start:
+		SPI_START
+		SendInstruction 'd'
+		SPI_STOP
+		mov arg,rcv
+		rcall LCD_Putchar
+		rcall LCD_Wait
+		dec con
+		brne start
+		ret
+
+;*****************************************************************
+;*	Configuración de la comunicación SPI en MASTER
+;*****************************************************************
+SPI_Minit:
+		;*	Set de SCK, MOSI y ~SS como salidas y MISO como
+		;*	entrada
+		sbi	DDRB,pMOSI
+		sbi	DDRB,pSCK
+		sbi	DDRB,pSS
+		;cbi	DDRB,pCSS
+		;*	Habilita comunicación SPI como MASTER a frecuencia
+		;*	de clock de f/16
+		ldi tmp, Cmaster
+		out SPCR, tmp
+		in tmp, SPSR
+		in tmp, SPDR
+		ret
+
+SPI_Sendstring:
+		ldi	con,0x00
+ssloop:	
+		ldi r30,0x00
+		ldi	r31,0x06
+		add r30,con
+		lpm	rtn,Z
+		out	spdr,rtn
+		in	dta,spdr
+		rcall SPI_Wait
+		ldi	r28,low(var)
+		ldi	r29,high(var)
+		add	r28,con
+		st	Y,dta
+		inc con
+		cpi	rtn,'g'
+		brne ssloop
+		ret
+
+
+
+
+
+;*****************************************************************
+;*	Operaciones de las Interrupciones
+;*****************************************************************
+SPI_STC:
+
+
+;*****************************************************************
+;*	Transmisión de 'tmt' por SPI al SLAVE
+;*****************************************************************
+SPI_Mtransmit:
+		out	SPDR, tmt
+		ret
+
+
+
+;*****************************************************************
+;*	Espera del fin de la recepción SPI
+;*****************************************************************
+SPI_Wait:
+		;*	Espera del fin de la recepción
+		in tmp, SPSR
+		sbrs tmp, SPIF
+		rjmp SPI_Wait
+		ret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;*****************************************************************
+;*	Rutinas para trabajar con el LCD
+;*****************************************************************
+
+LCD_Putstring:
+		ldi	con,0x00
+psloop:	
+		ldi r26,low(var)
+		ldi	r27,high(var)
+		add r26,con
+		ld	arg,X
+		mov tmt,arg
+		rcall LCD_Putchar
+		inc con
+		cpi	tmt,'f'
+		brne psloop
+		ret
+
+		
+
+LCD_init:
+	ldi		tmp, 0b00001100		;*las lineas de control son salidas y el resto entradas
+	out		DDRD, tmp
+	sbi		DDRC,1
+	rcall	LCD_delay				;*esperamos que inicie el lcd
+	
+	ldi		arg, 0x20			;*Le decimos al lcd que queremos usarlo en modo 4-bit
+	rcall	LCD_command8			;*ingreso de comando mientras aun esta en 8-bit
+  	rcall	LCD_wait
+
+	
+
+	ldi		arg, 0x28			;*Seteo: 2 lineas, fuente 5*7 ,
+	rcall	LCD_command				;
+	rcall	LCD_wait
+
+    
+
+	ldi		arg, 0x0C			;*Display on, cursor off
+	rcall	LCD_command
+	rcall	LCD_wait
+
+    
+		
+	ldi		arg, 0x01			;*borro display, cursor -> home
+	rcall	LCD_command
+	rcall	LCD_wait
+
+	
+
+	ldi		arg, 0x06			;*auto-inc cursor
+	rcall	LCD_command
+	ret
+
+
+;*	Used for init (we need some 8-bit commands to switch to 4-bit mode!)
+lcd_command8:
+		;we need to set the high nibble of DDRD while leaving
+		in	tmp, DDRD
+	
+		;the other bits untouched. Using tmp for that.
+		;set high nibble in tmp
+		sbr	tmp, 0b11110000
+		out	DDRD, tmp		;write value to DDRD again-> d7-d4 son salidas
+		in	tmp, PortD		;then get the port value, aparentemente portd esta en cero x defecto
+		cbr	tmp, 0b11110000	;borro the data bits 
+		cbr	arg, 0b00001111	;then clear the low nibble of the argument
+		;so that no control line bits are overwritten
+		or	tmp, arg		;then set the data bits (from the argument) in the
+		;Port value
+		/*en argument tengo el argumento con el nibble d las variables d control en cero*/
+		/*en tmp tngo los valores dl puerto d, con la parte d datos borrada*/
+		out	PortD, tmp		;and write the port value.
+		sbi	PortD, LCD_E		;now strobe E
+		nop
+		nop
+		nop
+		cbi	PortD, LCD_E
+		in	tmp, DDRD		;get DDRD to make the data lines input again
+		cbr	tmp, 0b11110000	;clear data line direction bits
+		out	DDRD, tmp		;and write to DDRD
+		ret
+
+lcd_putchar:
+		push arg;save the argmuent (it's destroyed in between)
+		in	tmp, DDRD	;get data direction bits
+		sbr	tmp, 0b11110000	;set the data lines to output
+		out	DDRD, tmp		;write value to DDRD
+		in	tmp, PortD		;then get the data from PortD
+		cbr	tmp, 0b11111110	;clear ALL LCD lines (data and control!)
+		cbr	arg, 0b00001111	;we have to write the high nibble of our argument first
+					;so mask off the low nibble
+		or	tmp, arg		;now set the argument bits in the Port value
+		out	PortD, tmp		;and write the port value
+		sbi	PortC, LCD_RS		;now take RS high for LCD char data register access
+		sbi	PortD, LCD_E		;strobe Enable
+		nop
+		nop
+		nop
+		cbi	PortD, LCD_E
+		pop	arg	;restore the argument, we need the low nibble now...
+		cbr	tmp, 0b11110000	;clear the data bits of our port value
+		swap	arg		;we want to write the LOW nibble of the argument to
+					;the LCD data lines, which are the HIGH port nibble!
+		cbr	arg, 0b00001111	;clear unused bits in argument
+		or	tmp, arg		;and set the required argument bits in the port value
+		out	PortD, tmp		;write data to port
+		sbi	PortC, LCD_RS		;again, set RS
+		sbi	PortD, LCD_E		;strobe Enable
+		nop
+		nop
+		nop
+		cbi	PortD, LCD_E
+		cbi	PortC, LCD_RS
+		in	tmp, DDRD
+		cbr	tmp, 0b11110000	;data lines are input again
+		out	DDRD, tmp
+		ret
+
+lcd_command:	;same as LCD_putchar, but with RS low!
+		push	arg;guardo la instruccion
+		in	tmp, DDRD;copio en tmp 00001110
+		sbr	tmp, 0b11110000; hago un or, entonces pongo en 1 el nibble alto
+		out	DDRD, tmp;pongo este valor en DDRD x lo tanto d7-d4 son salidas
+		in	tmp, PortD;guardo el valor dl puerto
+		cbr	tmp, 0b11111110;hago una and con 01,borro todos los bits
+		cbr	arg, 0b00001111;hago una and con F0, borro los 4 bits d control
+		or	tmp, arg;hago una or me quedan los 4 bits altos d la instruccion y cero debajo
+		out	PortD, tmp; lo cargo en el puerto
+		sbi	PortD, LCD_E
+		nop
+		nop
+		nop
+		cbi	PortD, LCD_E
+		pop	arg;saco el argumento d la pila
+		cbr	tmp, 0b11110000; hago una and con 0F, borro los dl nibble alto dejo los d control en cero
+		swap	arg;intercambio el nibble alto con el bajo
+		cbr	arg, 0b00001111;hago una and con F0,borro el nibble inferior d argument
+		or	tmp, arg;hago or con tmp
+		out	PortD, tmp;cargo en el puerto 
+		sbi	PortD, LCD_E
+		nop
+		nop
+		nop
+		cbi	PortD, LCD_E
+		in	tmp, DDRD
+		cbr	tmp, 0b11110000; vuelvo a poner el nibble alto en cero para q sea entrada.
+		out	DDRD, tmp
+		ret
+
+LCD_getaddr:	;works just like LCD_getchar, but with RS low, return.7 is the busy flag
+		in	tmp, DDRD
+		andi tmp, 0b00001111;esto es al pedo
+		out	DDRD, tmp
+		cbi	PortC, LCD_RS;pongo en cero para decir q es instruccion
+		sbi	PortD, LCD_RW; en uno para leer
+		sbi	PortD, LCD_E; seteo enable
+		nop
+		in	tmp, PinD;guardo lo q estaba en el pin
+		andi	tmp, 0b11110000;borro la parte d las variables d control
+		mov	rtn, tmp;pongo en return ese valor
+		cbi	PortD, LCD_E;pongo enable en cero
+		nop
+		nop;espero
+		sbi	PortD, LCD_E;pongo enable en uno
+		nop
+		in	tmp, PinD; guardo lo q esta en el pin, q es nibble bajo d la direccion, en realidad no lo necesito
+		andi	tmp, 0b11110000;borro la parte d las variables d control
+		swap	tmp; pongo el nibble alto en el bajo
+		or	rtn, tmp;hago un or con return,tngo la dire completa
+		cbi	PortD, LCD_E;pongo enable en cero
+		cbi	PortD, LCD_RW; pongo rw en cero
+		ret
+
+;*****************************************************************
+;*	Rutina de BUSY FLAG
+;*****************************************************************
+LCD_wait:				;read address and busy flag until busy flag cleared
+		rcall	LCD_getaddr
+		andi	rtn, 0x80;hago un and con 10000000
+		brne	LCD_wait;sale d aca solo cuando el busy flag es cero
+		ret
+
+
+;*****************************************************************
+;*	Rutina de DELAY
+;*	Duracion de 197ms
+;*****************************************************************
+LCD_delay:
+		clr	r2;pongo r2 en cero
+LCD_delay_outer:
+		clr	r3;pongo r3 en cero
+LCD_delay_inner:
+		dec r3;decremento r3
+		brne LCD_delay_inner; sale del ciclo si r3 es igual a cero
+		dec	r2;cuando ya hizo 255us decremento r2
+		brne LCD_delay_outer;sale dl ciclo si r2 es igual a cero
+		ret
+
+DELAY:
+	rcall LCD_delay
+	rcall LCD_delay
+	rcall LCD_delay
+	rcall LCD_delay
+	rcall LCD_delay
+	ret
+	
---
+++ shoots/master_slave_shifted/CristianSensorTry/slave/SLAVE__SPI_SENSOR.asm
@@ -0,0 +1,363 @@
+/*
+ * shift_slave.asm
+ *
+ *  Created: 29/06/2011 21:56:43
+ *   Author: alumno
+ */ 
+
+
+ ;*****************************************************************
+;*	File: EFCmicro1.asm
+;*	
+;*	Espectrofotocolorimetro Microcontrolador 1
+;*	
+;*	Autores:
+;*			- Rodriguez Cañete, Macarena
+;*			- Pepe, Ezequiel Ignacio
+;*			- Douce Suárez, Cristian Gabriel
+;*
+;*	
+;*
+;*
+;*
+;*
+
+	.include "m88def.inc"
+	.org 0
+;defino constantes
+	.equ	pCSS	=	1
+	.equ	pSS		=	2
+	.equ	pMOSI	=	3
+	.equ	pMISO	=	4
+	.equ	pSCK	=	5
+	.equ	Cslave =	0b11100000
+	
+	.equ sclk=4
+	.equ sdin=1
+	.equ sdout=0
+	.equ control=PORTD
+;defino simbolos
+	.def	tmp		=	r16
+	.def	arg		=	r17		;*	argument for calling subroutines
+	.def	rtn		=	r18		;*	rtn value from subroutines
+	.def    dta		=	r19		;*	el q le da el master
+	.def	tmt		=	r20
+	.def	rcv		=	r21
+	.def	usd		=	r22
+	.def delay=r23
+	.def pixel=r24
+
+/******************************************************************************/
+;Defino las variables
+ .dseg
+ var:	.byte	6
+
+	.org 0x0000
+	.cseg
+	rjmp RESET
+
+;*****************************************************************
+;*	Defino los vectores de interrupcion
+;*****************************************************************
+	.org 0x011
+		rjmp SPI_STC ; SPI Transfer Complete Handler
+
+;*****************************************************************
+;*	Inicialización del Micro luego del RESET
+;*****************************************************************
+RESET:
+		;*	Inicialización del StackPointer
+		ldi	tmp, low(RAMEND)
+		out	SPL, tmp
+		ldi	tmp, high(RAMEND)
+		out	SPH, tmp
+		ldi	r26,low(var);x apunta a var
+	    ldi	r27,high(var)
+		cbi DDRD, 0;configuro RX como entrada
+		sbi	DDRD, 1; TX como salida
+		sbi DDRD,4; XCK como salida,master mode
+
+
+		rjmp MAIN
+
+
+
+;*****************************************************************
+;*	MAIN Program for microcontroller
+;*****************************************************************
+MAIN:	rcall	SPI_Sinit
+		rjmp   idle	
+		
+idle:	sei
+		rjmp idle
+
+
+
+
+
+;*****************************************************************
+;*	Configuración de la comunicación SPI en SLAVE
+;*****************************************************************
+SPI_Sinit:
+		;*	Set MISO output, all others input
+		sbi DDRB,pMISO
+		sbi	DDRB,pCSS
+		;*	Habilita SPI, como SLAVE
+		ldi r17,Cslave
+		out SPCR,r17
+		in	tmp,SPSR
+		in	tmp,SPDR
+		ret
+
+;*****************************************************************
+;*	Operaciones de las Interrupciones
+;*****************************************************************
+SPI_STC:
+		in dta,SPDR;guardo el byte q me mando
+		cpi dta,'s';me fijo si fue la orden 's'
+		breq SENSOR_Init1;y voy a hacer la rutina dl sensor
+		cpi dta,'d'
+		breq SENSOR_Data
+return_interrupt:
+		reti
+
+SENSOR_Init1:
+		ldi tmp,'w'
+		out	SPDR,tmp
+		rcall sensor
+		ldi	r26,low(var);vuelvo a hacer q apunte al comienzo
+	    ldi	r27,high(var)
+		rjmp	return_interrupt
+
+SENSOR_Data:
+		ld r17,X+
+		out SPDR,r17
+		rjmp	return_interrupt	
+		
+sensor:
+		rcall sensor_init
+		rcall sensor_configuracion
+		ldi delay,0xE2
+		rcall timer;hago un delay d como minimo un milisegundo
+		;Start integration
+		ldi dta,0x08
+		rcall send
+		ldi delay,0xEA;pulso sclk 22 veces
+		rcall timer
+
+		ldi delay,0xF6;uso un tiempo d integracion d 2560us
+		rcall timer
+		;Detengo la integracion
+		ldi dta,0x10
+		rcall send
+		ldi delay,0xFB;pulso el sckl 5 veces
+		rcall timer
+
+		;Start Pixel Readout
+		;To initiate readout from the TSL3301, send the Start Read command and wait for a start bit on SDOUT:
+		ldi dta,0x02
+		rcall send
+		;ahora recibo los pixeles por la usart
+		ldi tmp,5;en realidad deberian ser 102
+        
+;mejorar esta parte
+recibir:
+		rcall USART_Receive
+	    st X+,pixel;guardo el pixel en la variable var
+	    dec tmp
+	    brne recibir
+
+		ldi tmp,'l'
+		st X,tmp
+		ret
+
+			
+		/***********************************************************/
+;Inicio el sensor con la usart apagada para poder controlar el valor de sdin
+;IMPORTANTE!
+;en la datasheet esta al reves, tal vez sea importante
+;cualquier cosa si no funciona lo doy vuelta
+
+sensor_init:
+           cbi control,sclk
+	       cbi control,sdin
+		   ldi tmp,0x04
+	       out TCCR0B,r20;divido la frecuencia por 256
+		   ldi tmp,0xE2
+	       out TCNT0,r20; pongo el timer en -30
+	       rcall clockmanual
+		   ldi tmp,0x04
+	       out TCCR0B,r20;divido la frecuencia por 256
+		   ldi tmp,0xF6;pongo el timer en -10
+		   out TCNT0,tmp
+		   sbi control,sdin
+		   rcall clockmanual
+		   rcall USART_Init;de ahora en mas no uso mas el clock manual sino el d la usart
+		   rcall sensor_reset
+		   ldi delay,0xFB; va a contar 5 clock
+		   rcall timer
+		   ;IMPORTANTE en la datasheet dice 1F,mientras q en el informe 5F,cualquier cosa se cambia
+		   ldi dta,0x1F;Write to mode register command;
+		   rcall send
+		   ldi dta,0x00;Clear mode register
+		   rcall send
+		   ret
+
+/********************************************************************************************************/
+;Esta rutina sirve sòlo para la inicializacion del sensor
+;porque despues se usa la señal xck d la usart
+;uso el timer ya pre escalado por 256
+;para que tenga una frecuencia similar a la que va a tener el sensor
+;cuando la usart le pase los dtas
+
+clockmanual: sbi control,sclk
+             cbi control,sclk  
+             sbis TIFR0,0
+             rjmp clockmanual
+			 cbi TIFR0,0;pongo d nuevo el flag en cero
+			 ldi arg,0x00
+	         out TCCR0B,arg;apago el timer
+			 ret
+/**************************************************************************************************************/
+
+USART_Init:
+;seteo el baud rate
+;importante:cuando lo uso en modo sincronico la frecuencia en xck
+;esta relacionada con el baud rate, xq por cada pulso d clock en 
+;xck se transmite un bit, x lo tanto una manera d saber el periodo
+;en xck es hacer 1/baudrate
+;Por eso es que cuando cuento los pulsos d sclk==xck,preescalo el timer por 256
+;ya que da una frecuencia parecida a la de la usart
+	ldi r17,0x00
+	ldi r16,0x0C; un baud rate de 4800 fosc=1MHz 
+	sts UBRR0H,r17
+	sts UBRR0L,r16
+	ldi r18,0b00100000; seteo el udre0
+	sts UCSR0A,r18
+	ldi r18,0b00011000;activo receptor y emisor
+	sts UCSR0B,r18
+	ldi r18,0b01000110;modo sincronico, 8 bits, sin bit d paridad
+	sts UCSR0C,r18;con 1 solo bit d stop
+	ret
+
+/************************************************************************************************************/
+;Rutina que realiza el reset
+;Lo puse aparte porque en el informe que presentaron tiene un delay extra,y ya que es la unica vez que lo presenta
+;preferi hacer una subrutina especial
+
+sensor_reset: ldi dta,0x1B;codigo dl reset
+              rcall USART_Transmit
+			  ldi delay,0xFA;espero 6 pulsos de clock
+			  rcall timer
+			  ret
+
+
+/*******************************************************************************************************************/
+;Rutina que envia un byte por la usart
+
+USART_Transmit:
+		;Wait for empty transmit buffer
+		lds	 r17,UCSR0A
+		sbrs r17,UDRE0
+		rjmp USART_Transmit
+		; Put data into buffer, sends the data
+		sts UDR0,dta
+		ret
+	
+/********************************************************************************************************************/
+
+USART_Receive:
+; Wait for data to be received
+	lds	 r17,UCSR0A
+	sbrs r17,RXC0
+	rjmp USART_Receive
+; Get and return received data from buffer
+	lds pixel, UDR0
+	ret
+
+/*************************************************************************************************************/
+;Utilizo el timer con una frecuencia aproximada a la de la usart para poder contar los pulsos de la señal
+;que va por sclk
+
+timer:ldi r20,0x04
+	  out TCCR0B,r20;divido la frecuencia por 256 y prendo el timer
+      out TCNT0,delay;digo cuanto voy a contar
+	  rcall contador
+	  ldi r20,0x00
+	  out TCCR0B,r20;apago el timer
+	  cbi TIFR0,0;pongo d nuevo el flag en cero
+	  ret
+
+/***************************************************************************************************************/
+;Contador espera a que se termine d contar y vuelve a la subrutina timer 	
+
+contador: sbis TIFR0,0
+          rjmp contador
+		  ret
+
+/*******************************************************************************************************************/
+;Rutina que envia dtas al sensor
+;ya incluye los pulsos de clock que necesita
+;el sensor para procesar las instrucciones
+
+send:    rcall USART_Transmit
+		 ldi delay,0xFA
+		 rcall timer;espera 6 clock para q se procese la instruccion
+		 ret
+
+/**********************************************************************************************************************/
+;Subrutina que sirve para configurar la ganancia y el offset
+;por las dudas voy a poner los valores normales
+
+sensor_configuracion:
+;In this example, we will set the gain to 0 and the offset to 0:
+        ;Left offset
+		ldi dta,0x40
+		rcall send
+		ldi dta,0x00;
+		rcall send
+		;Left gain
+		ldi dta,0x41 
+		rcall send
+		ldi dta,0x00
+		rcall send
+		;Middle offset
+		ldi dta,0x42
+		rcall send
+		ldi dta,0x00;
+		rcall send
+		;Middle gain
+		ldi dta,0x43
+		rcall send
+		ldi dta,0x00
+		rcall send
+		;Right offset
+		ldi dta,0x44
+		rcall send
+		ldi dta,0x00
+		rcall send
+		;Right gain
+		ldi dta,0x45
+		rcall send
+        ldi dta,0x00
+        rcall send
+		ret
+		
+		
+		
+;*****************************************************************
+;*	Rutina de delay para no pisar los bytes enviados
+;*****************************************************************
+	;*	Duracion de 197ms
+LCD_delay:
+		clr	r2;pongo r2 en cero
+LCD_delay_outer:
+		clr	r3;pongo r3 en cero
+LCD_delay_inner:
+		dec r3;decremento r3
+		brne LCD_delay_inner; sale del ciclo si r3 es igual a cero
+		dec	r2;cuando ya hizo 255us decremento r2
+		brne LCD_delay_outer;sale dl ciclo si r2 es igual a cero
+		ret
+
+
